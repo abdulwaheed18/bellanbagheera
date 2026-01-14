94b7bb857158897ed7e4750106280a963b3b4d1e
Abdul Waheed <abdulwaheed18@gmail.com>
2026-01-15 03:00:05 +0530

    added Amazon link

--- config.js
+++ config.js
@@ -13,25 +13,41 @@ const config = {
         { name: 'Amazon', url: 'https://www.amazon.in/shop/bella_and_bagheera', icon: 'images/icons/amazon.svg' }
     ],
     categories: [
-        { id: 'cat-gear', name: 'Cat Gear' },
+        { id: 'Cat-Bedding', name: 'Cat Bedding' },
         { id: 'home', name: 'Home' },
         { id: 'tech', name: 'Tech' }
     ],
     products: [
         {
-            category: 'cat-gear',
+            category: 'Cat-Bedding',
             image: "https://m.media-amazon.com/images/I/612h+CVqLoL._SX522_.jpg",
             title: "Cave Scratcher Cat House",
             url: "https://amzn.to/49m4Xx7",
             store: "Amazon"
         },
         {
-            category: 'tech',
+            category: 'Cat-Bedding',
             image: "https://m.media-amazon.com/images/I/71tuTXEji4L._SX679_.jpg",
             title: "Cactus Theme Cat Tree",
             url: "https://amzn.to/4jPnEwQ",
             store: "Amazon"
-        }
+        },
+                 {
+                     category: 'Cat-Bedding',
+                     image: "https://m.media-amazon.com/images/I/71kG0KRofQL._SX679_.jpg",
+                     title: "Multistory Cat Tree with Condos, Perches and Sctraching Post",
+                     url: "https://amzn.to/49VqPzq",
+                     store: "Amazon"
+                 }
+        ,
+                         {
+                             category: 'Cat-Bedding',
+                             image: "https://m.media-amazon.com/images/I/61gTOQJrkpL._SX522_.jpg",
+                             title: "Trampoline Cat Bed",
+                             url: "https://amzn.to/49RbgsF",
+                             store: "Amazon"
+                         }
+
     ],
     footerText: "© {year} Bella, Nawaab & Bagheera"
 };
